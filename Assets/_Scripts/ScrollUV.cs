83f31f3385e96c1d38fd5868f5ddeeedb3399ef9
Jasper van't Veen <jvanveen@gmail.com>
2016-04-24 13:37:15 +0200

    Structure

---
+++ Assets/_Scripts/ScrollUV.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollUV : MonoBehaviour {
+
+    MeshRenderer mr;
+    Material mat;
+    Vector2 offset;
+    GameObject spaceship;
+    int uvMoveSpeed = 20;
+    [Header("Change parallax effect. Higher means slower background movement")]
+    public float parralax = 2f;
+
+	// Use this for initialization
+	void Start () {
+        mr = GetComponent<MeshRenderer>();
+        mat = mr.materials[0];
+        offset = mat.mainTextureOffset;
+        spaceship = GameObject.FindGameObjectWithTag("Spaceship");
+    }
+
+    // Update is called once per frame
+    void Update () {
+        offset = spaceship.transform.position/ uvMoveSpeed / parralax;
+        mat.mainTextureOffset = offset;
+	}
+}
