b49c34c1fd34d7018fa2871284745105b8a41cd4
Jasper van't Veen <jvanveen@gmail.com>
2016-04-24 15:20:02 +0200

    no message

--- Assets/_Scripts/PlanetPicker.cs
+++ Assets/_Scripts/PlanetPicker.cs
@@ -12,7 +12,7 @@ public class PlanetPicker : MonoBehaviour {
         rb = gameObject.GetComponent<Rigidbody2D>();
         spaceshipRb = GameObject.FindGameObjectWithTag("Spaceship").GetComponent<Rigidbody2D>();
 
-		gameObject.GetComponent<SpriteRenderer> ().sprite = planetSkin[Random.RandomRange (0, planetSkin.Length - 1)];
+		gameObject.GetComponent<SpriteRenderer> ().sprite = planetSkin[Random.Range (0, planetSkin.Length - 1)];
         rb.velocity = new Vector2(0,planetSpeed);
 	}
 }
