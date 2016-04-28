338629ec7d165ad616886d50e7606d34c81b9b08
Jasper van't Veen <jvanveen@gmail.com>
2016-04-28 09:32:45 +0200

    Polygon instead of BoxCollider

--- Assets/_Scripts/DirtController.cs
+++ Assets/_Scripts/DirtController.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-[RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(PolygonCollider2D))]
 public class DirtController : MonoBehaviour {
     //private Scoreboard scoreboard;
 
