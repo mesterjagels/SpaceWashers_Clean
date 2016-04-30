985fdebb8d2d96e709bf749ab933e6c7dec7da1a
Jasper van't Veen <jvanveen@gmail.com>
2016-04-30 13:12:05 +0200

    multiplayer controls!

--- Assets/_Scripts/CleanerController.cs
+++ Assets/_Scripts/CleanerController.cs
@@ -1,16 +1,35 @@
 ﻿using UnityEngine;
 using System.Collections;
+using InControl;
 
 public class CleanerController : MonoBehaviour {
 
 	private Rigidbody2D rb;
 	public float force = 1;
     public KeyCode up, down, left, right;
+    private InputDevice device;
+    private InputControl control;
+    private int playerNumber;
+
+    void Awake () {
+        switch (gameObject.tag)
+        {
+            case "Player1":
+                playerNumber = 0;
+                break;
+
+            case "Player2":
+                playerNumber = 1;
+                break;
+
+            case "Player3":
+                playerNumber = 2;
+                break;
 
 
-	void Awake () {
-
+        }
 		rb = GetComponent<Rigidbody2D>();
+
 
 	}
 	
@@ -18,19 +37,19 @@ public class CleanerController : MonoBehaviour {
 	void Update () {
 	
 
-		if (Input.GetKey (up)) {
+		if (Input.GetKey (up) || InputManager.Devices[playerNumber].LeftStickUp) {
 			rb.velocity = new Vector2(rb.velocity.x, force);
 		}
 
-		if (Input.GetKey (left)) {
+		if (Input.GetKey (left) || InputManager.Devices[playerNumber].LeftStickLeft) {
 			rb.velocity = new Vector2(-force, rb.velocity.y);
 		}
 
-		if (Input.GetKey (right)) {
+		if (Input.GetKey (right) || InputManager.Devices[playerNumber].LeftStickRight) {
 			rb.velocity = new Vector2(force, rb.velocity.y);
 		}
 
-		if (Input.GetKey (down)) {
+		if (Input.GetKey (down) || InputManager.Devices[playerNumber].LeftStickDown) {
 			rb.velocity = new Vector2(rb.velocity.x, -force);
 
 		}
