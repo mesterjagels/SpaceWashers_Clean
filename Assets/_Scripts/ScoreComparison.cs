5cc32a8f2b946f465f46a52cafb54cb354c5b541
Leonhard Pickny <Leonhard.Pickny@gmail.com>
2016-05-10 01:34:05 +0200

    Displays the team score comparison after the game ends and switches to the highscore screen when the space key is hit.

---
+++ Assets/_Scripts/ScoreComparison.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreComparison : MonoBehaviour {
+
+    private GameManager gameManager;
+
+    public Text scoreText0;
+    public Text scoreText1;
+    public Text scoreText2;
+    public Text scoreText3;
+    public Text scoreText4;
+    
+
+    void Start()
+    {
+        gameManager = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameManager>();
+
+        //UI Elements
+        scoreText0.text = "TOTAL SCORE" + gameManager.totalScore;
+        scoreText1.text = "CAPTAIN DUDE" + gameManager.captainScore;
+        scoreText2.text = "FEMALE DUDE" + gameManager.cleanerScore[0];
+        scoreText3.text = "BLACK DUDE" + gameManager.cleanerScore[1];
+        scoreText4.text = "TOILETBRUSH DUDE" + gameManager.cleanerScore[2];
+    }
+
+    void Update()
+    {
+
+    }
+}
