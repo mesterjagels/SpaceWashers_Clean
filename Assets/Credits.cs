ac84d2536535e3cb178dcc3d3ad1e8ae225d61bc
Jasper van't Veen <jvanveen@gmail.com>
2016-05-15 19:24:13 +0200

    Credits scene

---
+++ Assets/Credits.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using InControl;
+
+public class Credits : MonoBehaviour {
+	// Update is called once per frame
+	void Update () {
+        if (InputManager.Devices[0].AnyButton || InputManager.Devices[1].AnyButton || InputManager.Devices[2].AnyButton)
+        {
+            Application.LoadLevel(0);
+        }	
+	}
+}
