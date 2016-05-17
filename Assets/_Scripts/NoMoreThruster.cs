5cb54a6140f5c3c70e86bdd1f87b2f3dcc484f1c
Jasper van't Veen <jvanveen@gmail.com>
2016-05-17 19:21:05 +0200

    No more thrusters at certain point of arrival (0,5 distance left)

---
+++ Assets/_Scripts/NoMoreThruster.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NoMoreThruster : MonoBehaviour {
+
+    private GameManager gm;
+    private ParticleSystem ps;
+    
+	void Start () {
+        gm = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameManager>();
+        ps = GetComponent<ParticleSystem>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	if(gm.distanceMath < 0.5f)
+        {
+            if(ps.enableEmission == true) {
+                ps.enableEmission = false;
+            }
+        }
+	}
+}
